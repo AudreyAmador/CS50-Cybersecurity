98a235085d95d0fd4735cfb0c259e9891522b5c2
Audrey Amador <aamador@baccredomatic.cr>
2024-09-29 17:00:17 -0600

    git branch -M main

---
+++ Cybersecurity/Crack2.py
@@ -0,0 +1,11 @@
+#pass with 4 letters
+
+from string import ascii_letters
+
+for i in ascii_letters:
+    for j in ascii_letters:
+        for k in ascii_letters:
+            for l in ascii_letters:
+                print(i,j,k,l) 
+
+
